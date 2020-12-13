c88f849051657155af45bea7bff478dc685313d9
Tarik <33683238+Tarikyil1998@users.noreply.github.com>
2020-12-13 12:00:54 +0100

    added comments

--- src/FloodFillAlgorithmJob.java
+++ src/FloodFillAlgorithmJob.java
@@ -10,7 +10,7 @@ public class FloodFillAlgorithmJob  implements Runnable{
     private int intervalSize;
     private int startPosition;
 
-
+    //Implementing Flood Fill Job. Which the threads will use.
     FloodFillAlgorithmJob(String name, int[][] input, int intervalSize, int startPosition){
         threadName = name;
 
@@ -29,18 +29,21 @@ public class FloodFillAlgorithmJob  implements Runnable{
             x--;
         if (y!= 0)
             y--;
-
+        //Make a queue with points. Points have a X and Y value.
         Queue<Point> queue = new LinkedList<Point>();
+
         if (screen[y][x] != original){
             return;
         }
         queue.add(new Point(x, y));
-
+        // Loop till queue is empty
         while (!queue.isEmpty()){
             Point p = queue.remove();
+            // Make sure points does not exceed max input size.
             if(p.x < 0 || p.x >= screen.length || p.y < 0 || p.y >= screen.length){
                 continue;
             }
+            // if the value of the point is 0 change that to 3 to fill the area
                 if (screen[p.y][p.x] == original){
                 screen[p.y][p.x] = fill;
                 queue.add(new Point(p.x-1, p.y));
--- src/FloodFillSequential.java
+++ src/FloodFillSequential.java
@@ -11,18 +11,20 @@ class FloodFillSequential {
             x--;
         if (y!= 0)
             y--;
-
+        // Make a queue with points. Points have a X and Y value.
         Queue<Point> queue = new LinkedList<Point>();
         if (screen[y][x] != original){
             return;
         }
         queue.add(new Point(x, y));
-
+        // Loop till queue is empty
         while (!queue.isEmpty()){
             Point p = queue.remove();
+            // Make sure points does not exceed max input size.
             if(p.x < 0 || p.x >= screen.length || p.y < 0 || p.y >= screen.length){
                 continue;
             }
+            // if the value of the point is 0 change that to 3 to fill the area
             if (screen[p.y][p.x] == original){
                 screen[p.y][p.x] = fill;
                 queue.add(new Point(p.x-1, p.y));
@@ -32,6 +34,7 @@ class FloodFillSequential {
             }
 
         }
+        //Print out the run time of the algorithm
         long elapsetime = System.nanoTime();
         System.out.println("Elapse time sequential approach : " +(elapsetime-starttime));
         return;
